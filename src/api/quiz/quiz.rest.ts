72001ddc503d79c7115795fca02877c5fd32f9f7
minh6825 <minhnqdeveloper@gmail.com>
2024-12-09 23:30:46 +0700

    update timeout

--- src/api/quiz/quiz.rest.ts
+++ src/api/quiz/quiz.rest.ts
@@ -3,12 +3,12 @@ import axios from "axios";
 import axiosConfig from "../axiosConfig";
 
 export interface ITagCount {
-    [tag: string]: number; 
-  }
-  
-  export interface ITagCountBySection {
-    [section: string]: ITagCount; 
-  }
+  [tag: string]: number;
+}
+
+export interface ITagCountBySection {
+  [section: string]: ITagCount;
+}
 
 export interface IQuiz {
   quizId: string;
@@ -22,28 +22,28 @@ export interface IQuiz {
 }
 
 export interface IQuizDetail {
-    quizId: string,
-    title: string,
-    description: string,
-    isCompleted: boolean,
-    totalTime: number,
-    sectionNumber: number,
-    questionNumber: number,
-    tagCountBySection: ITagCountBySection
+  quizId: string;
+  title: string;
+  description: string;
+  isCompleted: boolean;
+  totalTime: number;
+  sectionNumber: number;
+  questionNumber: number;
+  tagCountBySection: ITagCountBySection;
 }
 
 export interface IReturnBase {
-    success: boolean;
-    message: string;
+  success: boolean;
+  message: string;
 }
 
 export interface IReturnQuizListPublic extends IReturnBase {
-  quizzes: IQuiz[]
-  quantityQuizTest: number
+  quizzes: IQuiz[];
+  quantityQuizTest: number;
 }
 
 export interface IReturnQuizPublic extends IReturnBase {
-    quiz: IQuizDetail
+  quiz: IQuizDetail;
 }
 
 export const getQuizListPublicApi = async (params: {
@@ -56,12 +56,27 @@ export const getQuizListPublicApi = async (params: {
   quizSkill?: string;
 }): Promise<IReturnQuizListPublic> => {
   const query = new URLSearchParams(params as any).toString();
-  console.log(query)
-  return (await axios.get(`${NEXT_PUBLIC_SERVER}/quizzes/quiz-public-list?${query}`)).data;
-}
+  console.log(query);
+  return (
+    await axios.get(`${NEXT_PUBLIC_SERVER}/quizzes/quiz-public-list?${query}`)
+  ).data;
+};
 
-export const getDetailQuizApi = async (quizId: string): Promise<IReturnQuizPublic> => (await axios.get(`${NEXT_PUBLIC_SERVER}/quizzes/details/${quizId}`)).data
+export const getDetailQuizApi = async (
+  quizId: string
+): Promise<IReturnQuizPublic> =>
+  (
+    await axios.get(`${NEXT_PUBLIC_SERVER}/quizzes/details/${quizId}`, {
+      timeout: 10000,
+    })
+  ).data;
 
-export const getQuizTodoApi = async (quizId: string, token: string) => (await axios.get(`${NEXT_PUBLIC_SERVER}/quizzes/to-do/${quizId}`, {headers: {
-    Authorization: `Bearer ${token}`
-}})).data
+export const getQuizTodoApi = async (quizId: string, token: string) =>
+  (
+    await axios.get(`${NEXT_PUBLIC_SERVER}/quizzes/to-do/${quizId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      timeout: 10000,
+    })
+  ).data;
--- src/api/user/auth.rest.ts
+++ src/api/user/auth.rest.ts
@@ -2,9 +2,13 @@ import axios from "axios"
 
 
 export const loginGoogleApi = async () => {
-    return axios.get('http://localhost:8000/api/v1/auth/google-login')
+    return axios.get('http://localhost:8000/api/v1/auth/google-login', {
+        timeout: 10000,
+    })
 }
 
 export const googleRedirectApi = async () => {
-    return axios.get('http://localhost:8000/api/v1/auth/google-redirect')
+    return axios.get('http://localhost:8000/api/v1/auth/google-redirect', {
+          timeout: 10000,
+    })
 }
